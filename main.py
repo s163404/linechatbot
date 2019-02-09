9044509a7b13fda49855a14f2a864e2f380f3062
s163404 <s163404@gmail.com>
2019-02-09 23:06:35 +0900

    port setting changed main.py

--- main.py
+++ main.py
@@ -48,6 +48,5 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    app.run()
-#     port = int(os.environ.get('PORT', 5000))
-#     app.run(port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(port=port)
