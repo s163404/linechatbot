3199c25916ae15adf8de0ad54cc7bee76e240779
s163404 <s163404@gmail.com>
2019-02-11 01:47:24 +0900

    echo reply improve main.py

--- main.py
+++ main.py
@@ -54,7 +54,12 @@ def handle_message(event):
     # 拾うあいさつ集を作って辞書化する ->後日実装
     if text == 'おはよう' or 'こんにちは' or 'こんばんは':
         temp = "fsouhs"
-    elif text == "も":
+    #     hour_now = datetime.datetime.now().hour + 9 
+    #     reply_text = 'おは！' if hour_now >= 4 and hour_now <= 10 else 'お昼食べた？' if hour_now >= 11 and hour_now <= 16 else 'こんばんは'
+    #     line_bot_api.reply_message(
+    #         event.reply_token,
+    #         TextSendMessage(text=reply_text))
+    if text == "も":
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=event.message.text))
